065baad3c4155245bb0aeab10dc1a5de9065f1b8
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-06-01 14:14:07 -0700

    added questions and started init() function

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-const questions = [
+const fs = require("fs");
+const questions = ["What is your GitHub username?"];
 
-];
+const md = [];
 
 function writeToFile(fileName, data) {
+  fs.writeFile("./README.md", md.trim(), (err) => {
+    if (err) {
+      throw err;
+    }
+    console.err;
+  });
 }
 
-function init() {
-
-}
+function init() {}
 
 init();
