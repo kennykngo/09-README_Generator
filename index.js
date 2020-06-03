087444decc82419b6fb32467143ea85c2fe7182d
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-06-02 18:36:46 -0700

    added functionality

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
 const fs = require("fs");
-const questions = ["What is your GitHub username?"];
+const axios = require("axios");
+const inquirer = require("inquirer");
 
-const md = [];
+const questions = [
+  {
+    type: "input",
+    name: "github",
+    message: "Enter GitHub username.",
+  },
+];
 
-function writeToFile(fileName, data) {
-  fs.writeFile("./README.md", md.trim(), (err) => {
-    if (err) {
-      throw err;
-    }
-    console.err;
+// function writetoFile(fileName, data) {}
+
+function init() {
+  inquirer.prompt(questions).then(function (res) {
+    // console.log(res);
+    axios
+      .get(`https://api.github.com/users/${res.github}`)
+      .then(function (res) {
+        console.log(res);
+        var README = `
+# username: ${res.data.login}
+    
+url: ${res.data.url}
+\n
+avatar url: ${res.data.avatar_url}
+
+      `;
+
+        return README;
+      })
+      .then(function (data) {
+        fs.writeFileSync("./README.md", data, (err) => {
+          if (err) throw err;
+          // console.log(err);
+          console.log("failed");
+        });
+      });
   });
 }
-
-function init() {}
-
 init();
